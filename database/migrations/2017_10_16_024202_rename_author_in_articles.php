303f57146d3b87e23518d1b9995cf5d5fcc04619
Loendz <hadist.lm@gmail.com>
2017-10-16 10:15:38 +0700

    Update Migrations

--- database/migrations/2017_10_16_024202_rename_author_in_articles.php
+++ database/migrations/2017_10_16_024202_rename_author_in_articles.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddAuthorToArticles extends Migration
+class RenameAuthorInArticles extends Migration
 {
     /**
      * Run the migrations.
@@ -14,7 +14,7 @@ class AddAuthorToArticles extends Migration
     public function up()
     {
         Schema::table('articles', function (Blueprint $table) {
-            $table->string("author")->after("id");
+            $table->renameColumn("author", "writer")
         });
     }
 
@@ -26,7 +26,7 @@ class AddAuthorToArticles extends Migration
     public function down()
     {
         Schema::table('articles', function (Blueprint $table) {
-            $table->dropColumn('author');
+            $table->renameColumn("writer", "author");
         });
     }
 }
